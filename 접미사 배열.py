6467a9a2b0f02616be542f3dc91881793ae13738
SangHui48 <vodlrm123@naver.com>
2023-06-21 04:57:35 +0900

    6/21

---
+++ 접미사 배열.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181909
+
+def solution(my_string):
+    answer = []
+    for i in range(len(my_string)):
+        answer.append(my_string[i:])
+    return sorted(answer)
